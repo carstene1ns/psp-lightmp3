8119fa27337a03eb1b2018923028d9211aa5ac79
Paolo Iommarini <sakya@users.noreply.github.com>
2007-11-15 20:47:03 +0000

    uploaded 1.6.0 BETA 1

---
+++ src/m3u.h
@@ -0,0 +1,46 @@
+//    LightMP3
+//    Copyright (C) 2007 Sakya
+//    sakya_tg@yahoo.it
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+//M3U header file
+#define MAX_SONGS 400
+
+struct M3U_songEntry{
+	char fileName[262];
+	int length;
+	char title[257];
+};
+
+struct M3U_playList{
+	char fileName[262];
+	int songCount;
+	struct M3U_songEntry songs[MAX_SONGS];
+	int modified;
+};
+
+int M3U_open(char *fileName);
+int M3U_save(char *fileName);
+int M3U_getSongCount();
+int M3U_getTotalLength();
+int M3U_moveSongUp();
+int M3U_moveSongDown();
+int M3U_removeSong(int index);
+int M3U_addSong(char *fileName, int length, char *title);
+struct M3U_songEntry M3U_getSong(int index);
+int M3U_isModified();
+int M3U_forceModified(int modified);
+int M3U_clear();
